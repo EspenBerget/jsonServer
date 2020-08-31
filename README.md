b3b6aa3dbf3e83860f39c0e8a8d2c47dee1ce7b6
EspenBerget <espen.berget@tuta.io>
2020-08-31 10:51:55 +0200

    Added cleaner error handeling

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # JSON server in Go
 
 ### TODO
-* add test to handlers
-* change error messages in handler to log and also so user gets more relevant information
-* add form validation
-* make index return json if requested
+* Add test to handlers
+* Add form validation
+* Add TLS support
--- server.go
+++ server.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+func checkInternalError(err error) {
+	if err != nil {
+		panic(http.ErrAbortHandler)
+	}
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
@@ -19,39 +24,23 @@ func jsonGetID(w http.ResponseWriter, r *http.Request) {
 	vs := mux.Vars(r)
 	idString := vs["id"]
 	id, err := strconv.Atoi(idString)
-	if err != nil {
-		em := NewError("user error", "Misformed request id, must be a number.")
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
+
 	s, err := getID(db, int64(id))
-	if err != nil {
-		em := NewError("internal", err.Error())
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
+
 	b, err := json.Marshal(s)
-	if err != nil {
-		em := NewError("internal", err.Error())
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
+
 	fmt.Fprint(w, string(b))
 }
 
 func jsonGetAll(w http.ResponseWriter, r *http.Request) {
 	ss, err := getAll(db)
-	if err != nil {
-		em := NewError("internal", err.Error())
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
+
 	b, err := json.Marshal(ss)
-	if err != nil {
-		em := NewError("internal", err.Error())
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
 
 	fmt.Fprint(w, string(b))
 }
@@ -60,26 +49,15 @@ func jsonAdd(w http.ResponseWriter, r *http.Request) {
 	s := Suggestion{}
 	b := make([]byte, 1024)
 	n, err := r.Body.Read(b)
-	if err != io.EOF && err != nil {
-		em := NewError("internal", "Error reading body")
-		log.Println(err)
-		fmt.Fprint(w, em)
-		return
+	if err != io.EOF {
+		checkInternalError(err)
 	}
+
 	b = b[:n]
-	if err := json.Unmarshal(b, &s); err != nil {
-		em := NewError("internal", "Error parsing data in body")
-		log.Println(err)
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(json.Unmarshal(b, &s))
+
 	id, err := add(db, s)
-	if err != nil {
-		em := NewError("internal", "Error adding data")
-		log.Println(err)
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
 
 	fmt.Fprintf(w, "Added idea '%s' for IDE '%s' with ID %d", s.Idea, s.IDE, id)
 }
@@ -88,16 +66,9 @@ func jsonDelete(w http.ResponseWriter, r *http.Request) {
 	vs := mux.Vars(r)
 	idString := vs["id"]
 	id, err := strconv.Atoi(idString)
-	if err != nil {
-		em := NewError("user error", "Misformed request id, must be a number.")
-		fmt.Fprint(w, em)
-		return
-	}
-	if err := delete(db, int64(id)); err != nil {
-		em := NewError("internal", fmt.Sprintf("Failed to delete id: %d, error message: %v", id, err))
-		fmt.Fprint(w, em)
-		return
-	}
+	checkInternalError(err)
+
+	checkInternalError(delete(db, int64(id)))
 
 	fmt.Fprintf(w, "Id %d deleted", id)
 }
